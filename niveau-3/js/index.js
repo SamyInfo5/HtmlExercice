a15282e3658c74020f70d841b14a03091dbe985f
Samuel <samueldhondt@MacBook-Air-de-Samuel.local>
2023-07-24 16:47:36 +0200

    ajustement

--- niveau-3/js/index.js
+++ niveau-3/js/index.js
@@ -4,10 +4,10 @@ let navlink = document.querySelectorAll('a')
 window.onscroll = () => {
     section.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 2;
+        let offset = sec.offsetTop - 650;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id')
-        console.log(top, offset, height, id)
+       
 
         if ( top >= offset && top < offset + height)  {
             navlink.forEach(links => {
