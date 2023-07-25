370cf7e04394d7af33deca780ca3f950ad5190c5
Samuel <samueldhondt@MacBook-Air-de-Samuel.local>
2023-07-25 12:15:16 +0200

    navbar active

--- niveau-3/js/index.js
+++ niveau-3/js/index.js
@@ -1,19 +1,24 @@
-let section = document.querySelectorAll("section")
-let navlink = document.querySelectorAll('a')
+/*navigation bar link active */
+const section = document.querySelectorAll("section")
+const navlink = document.querySelectorAll('header nav ul li a')
+/*Variable for bg bluring and opacity scrolling*/
+const scrolling = document.getElementsByClassName('scrolling')
+const back = document.getElementById('Acceuil')
 
 window.onscroll = () => {
     section.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 650;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id')
-       
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+        
 
         if ( top >= offset && top < offset + height)  {
+            
             navlink.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
             })
-        }
+        };
     });
 }
