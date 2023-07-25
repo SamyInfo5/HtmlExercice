eb4949d6fb4b9aa59cfad682a8cba3fcf1d6844d
Samuel <samueldhondt@MacBook-Air-de-Samuel.local>
2023-07-25 15:59:15 +0200

    ajout js

--- niveau-3/js/index.js
+++ niveau-3/js/index.js
@@ -2,18 +2,23 @@
 const section = document.querySelectorAll("section")
 const navlink = document.querySelectorAll('header nav ul li a')
 /*Variable for bg bluring and opacity scrolling*/
-const scrolling = document.getElementsByClassName('scrolling')
-const back = document.getElementById('Acceuil')
+const title = document.getElementById("title-hero")
+const subtitle = document.getElementById("subtitle-hero")
+const net = document.getElementById('bg-net')
+
+console.log(title.style)
 
 window.onscroll = () => {
+    const Value = window.scrollY;
+    net.style.opacity = (100 - Value/2)  / 100;
+    title.style.transform = `translateY(${Value*1.25}px)`
+    subtitle.style.transform = `translateY(${Value*1.25}px)`
     section.forEach(sec => {
-        const top = window.scrollY;
         const offset = sec.offsetTop - 150;
         const height = sec.offsetHeight;
         const id = sec.getAttribute('id');
         
-
-        if ( top >= offset && top < offset + height)  {
+        if ( Value >= offset && Value < offset + height)  {
             
             navlink.forEach(links => {
                 links.classList.remove('active');
